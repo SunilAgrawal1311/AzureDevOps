60ae36226cb9ad473f46b954d654b41c65b5af15
SunilAgrawal1311 <sunilagrawal2006@gmail.com>
2024-04-08 19:22:07 +0530

    Added NET 7 Project

---
+++ AzureNET7API/AzureNET7API/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using AzureNET7API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureNET7API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public SuperHeroController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get() 
+        {
+            List<SuperHero> list = new List<SuperHero>();
+            list.Add(new SuperHero { Id = 1, Name = "SpiderMan", FirstName = "Peter", LastName = "Parkar", City = "New York" });
+            list.Add(new SuperHero { Id = 2, Name = "ShaktiMan", FirstName = "Mukesh", LastName = "Khanna", City = "Mumbai" });
+
+            return Ok(list);
+
+        }
+    }
+}
